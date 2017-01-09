22704213421affe79b64c77ca291e9f6bab66cc6
Paul Kremer <krepa098@gmail.com>
2017-01-09 17:12:14 +0100

    load parameters from the ROS parameter service instead of args

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,23 @@
 
 int main(int argc, char** argv)
 {
-    const std::vector<std::string> args(argv + 1, argv + argc);
-
-    // make sure we have a config file
-    assert(args.size() > 1);
-
     // init ros
     ros::init(argc, argv, "atlas");
 
+    // get config directory
+    ros::NodeHandle n("~");
+    auto configFile = n.param<std::string>("config", "");
+
+    assert(!configFile.empty());
+
     // init atlas
-    Config config(args[1]);
+    Config config(configFile);
     SensorListener sensorListener(config);
     TransformGraph graph;
     TransformGraphBroadcaster broadcaster;
+
+    // print the config
+    config.dump();
 
     //////////////////////////////////////
     ///      Main Loop
